2c6a943627075f8d9f88817526180c9789a2745e
Edilson <edilson.a.andrade@gmail.com>
2026-07-10 16:57:44 -0300

    feat: change  vector manager to use right chroma

--- modules/vetorizacao/vector_manager.py
+++ modules/vetorizacao/vector_manager.py
@@ -1,23 +1,23 @@
+# modules/vectorization/vector_manager.py
 import os
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+# CORREÇÃO: Usando o pacote atualizado e definitivo recomendado pelo LangChain
+from langchain_chroma import Chroma
 
 class VectorManager:
     def __init__(self, db_directory: str):
         """
-        Initializes the vector database manager for a specific directory
+        Initializes the vector database manager for a specific directory.
         """
-        self.db_directory = db_directory;
-        print(f"Initializing Multilingual Embedding form {self.db_directory}...")
+        self.db_directory = db_directory
+        print(f"Initializing Multilingual Embeddings for: {self.db_directory}...")
         self.embeddings = HuggingFaceEmbeddings(model_name="paraphrase-multilingual-MiniLM-L12-v2")
-        
         self.db = None
         
         if os.path.exists(self.db_directory):
             self.db = Chroma(persist_directory=self.db_directory, embedding_function=self.embeddings)
-        
-        
-    def create_db_with_texts( self, texts: list):
+
+    def create_db_with_texts(self, texts: list):
         """
         Converts texts into vectors and saves them to disk.
         """
@@ -36,6 +36,7 @@ class VectorManager:
         if not self.db:
             raise ValueError(f"The database at {self.db_directory} is not initialized or empty.")
         
+        # O MMR vai buscar candidatos e filtrar os mais diversos
         results = self.db.max_marginal_relevance_search(
             query, 
             k=num_results,
@@ -43,4 +44,3 @@ class VectorManager:
         )
         
         return [doc.page_content for doc in results]
-        
